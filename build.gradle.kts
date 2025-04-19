3ea173fea76811ff2701e734b54d26c6f4e685fe
James O'Cull <james.ocull@gmail.com>
2025-04-19 14:08:12 -0400

    Initial project and setup

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.codefromjames"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/codefromjames/vector/Main.java
@@ -0,0 +1,12 @@
+package com.codefromjames.vector;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello, World!");
+    }
+}
