5c85e88769f258468ca6f97b0f23508969d7f373
James O'Cull <james.ocull@gmail.com>
2025-04-29 17:21:11 -0400

    Trying more sparse vectors to test searching

--- src/main/java/com/codefromjames/vector/HNSWExample.java
+++ src/main/java/com/codefromjames/vector/HNSWExample.java
@@ -409,32 +409,9 @@ public class HNSWExample {
     public static void main(String[] args) {
         final HNSWIndex index = new HNSWIndex();
 
-//        final double[][] data = {
-//                {1.0, 2.0, 3.0},
-//                {4.0, 5.0, 6.0},
-//                {7.0, 8.0, 9.0},
-//                {2.0, 3.0, 4.0},
-//                {5.0, 6.0, 7.0},
-//                {8.0, 9.0, 10.0},
-//                {3.0, 4.0, 5.0},
-//                {6.0, 7.0, 8.0},
-//                {9.0, 10.0, 11.0},
-//                {4.0, 5.0, 6.0},
-//                {10.0, 11.0, 12.0},
-//                {5.0, 6.0, 7.0},
-//                {11.0, 12.0, 13.0},
-//                {6.0, 7.0, 8.0},
-//                {12.0, 13.0, 14.0},
-//                {7.0, 8.0, 9.0},
-//                {13.0, 14.0, 15.0},
-//                {8.0, 9.0, 10.0},
-//                {14.0, 15.0, 16.0},
-//                {9.0, 10.0, 11.0},
-//        };
-
-        final int vectorSize = 2;
+        final int vectorSize = 10;
         LOGGER.info("Vectors generating... vector size = {}", vectorSize);
-        final List<double[]> data = IntStream.range(0, 1_000_000)
+        final List<double[]> data = IntStream.range(0, 1000)
                 .mapToObj(i -> randomVector(vectorSize))
                 .toList();
 
